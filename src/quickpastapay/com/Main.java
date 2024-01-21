8f683f4129ed2b294715e8cefda2a379588907da
codelove <abdurrahmaanbica123@gmail.com>
2024-01-21 16:27:04 +0100

    Setting up the QuickPastaPay project.

---
+++ src/quickpastapay/com/Main.java
@@ -0,0 +1,13 @@
+package quickpastapay.com;
+
+import quickpastapay.com.Dish.DishManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        DishManagement dishManagement = new DishManagement();
+        dishManagement.showMenu();
+
+
+    }
+
+}
